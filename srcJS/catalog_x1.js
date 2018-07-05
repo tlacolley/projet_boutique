7719887f76f121d6c4e68c2ccfa5016fea3ac316
tlacolley <thomaslacolley@gmail.com>
2018-07-05 13:57:47 +0200

    ajout folder Js

---
+++ srcJS/catalog_x1.js
@@ -0,0 +1,11 @@
+
+var product0 = {
+    name : "Produit 0",
+    description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et egestas augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum maximus eros, a condimentum diam dapibus non. Ut tempus sapien at blandit hendrerit. Mauris vel vehicula orci. Donec non risus eu diam accumsan ornare a vel nunc. Etiam porta enim nisi, sit amet imperdiet lectus commodo eget. Nulla magna ex, dapibus sed rhoncus nec, auctor et ligula. In malesuada ante neque, quis condimentum sapien faucibus eget. Vestibulum in interdum ipsum. Duis ut lorem velit. Aenean congue cursus neque, eget egestas turpis mattis non. Mauris quis diam aliquet, blandit justo id, cursus eros. Sed et vulputate velit. Integer finibus lacus vel porta laoreet.",
+    price : 35,
+    quantity : 4,
+    thumb : "http://placehold.it/140x140",
+    pictures : ["http://placehold.it/400x400", "http://placehold.it/400x400", "http://placehold.it/400x400", "http://placehold.it/400x400"],
+}
+    
+var catalog = [product0 ,]
